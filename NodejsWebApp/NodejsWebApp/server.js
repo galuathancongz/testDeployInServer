b20b95e508ee7d53cc891a3d602d8cbabaf3ca7d
galuathancongz <93344770+galuathancongz@users.noreply.github.com>
2024-01-26 00:34:09 +0700

    Update server.js

--- NodejsWebApp/NodejsWebApp/server.js
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// ??nh ngh?a m?t route ??n gi?n
 app.get('/', (req, res) => {
     res.send('HelloWord!');
 });
 
-// B?t ??u l?ng nghe trên c?ng ?ã ch?n
 app.listen(port, () => {
     console.log(`ServerRunning in :${port}`);
 });
