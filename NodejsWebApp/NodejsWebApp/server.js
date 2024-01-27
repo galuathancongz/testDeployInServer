e1dda67e7a1ca773eb3519f5aa0bc00387a45532
galuathancongz <93344770+galuathancongz@users.noreply.github.com>
2024-01-27 15:41:52 +0700

    Update server.js

--- NodejsWebApp/NodejsWebApp/server.js
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('HelloWord!');
+app.get('/apitest', (req, res) => {
+    res.send('HelloWord! Day la API test');
 });
 //
 app.listen(port, () => {
